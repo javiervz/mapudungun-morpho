a9be65505e0182493077e315efbc773e0d0056a8
javier vera <jxvera@gmail.com>
2021-05-26 12:23:16 -0400

    bold

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ def verb_to_mapudungun(n_clicks, verb_esp,numero,persona,polaridad,tiempo):
                 traduccion=traduccion+'yngün'
 
 
-    return (html.P(['En mapudungun, el verbo "{}" conjugado en "{}" persona "{}" en polaridad "{}" y tiempo "{}" se dice'.format(verb_esp,persona,numero,polaridad,tiempo)+' '+'"'+traduccion+'"',html.Br(),html.Strong('Morfología :) '+expansion)]))
+    return (html.P(['En mapudungun, el verbo "{}" conjugado en "{}" persona "{}" en polaridad "{}" y tiempo "{}" se dice'.format(verb_esp,persona,numero,polaridad,tiempo)+' '+'"'+html.Strong(traduccion)+'"',html.Br(),html.Strong('Morfología :) '+expansion)]))
 
 
 
