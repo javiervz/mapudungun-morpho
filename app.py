e418a57d0eb313facd75fd57cc8ff8e2d24f226a
javier vera <jxvera@gmail.com>
2021-05-26 08:55:25 -0400

    markdown

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def verb_to_mapudungun(n_clicks, verb_esp,numero,persona,polaridad,tiempo):
                 traduccion=traduccion+'yngün'
 
 
-    return 'En mapudungun, el verbo "{}" conjugado en "{}" persona "{}" en polaridad "{}" y tiempo "{}" se dice'.format(verb_esp,persona,numero,polaridad,tiempo)+' '+'"'+traduccion+'"'+' '+expansion
+    return 'En mapudungun, el verbo "{}" conjugado en "{}" persona "{}" en polaridad "{}" y tiempo "{}" se dice'.format(verb_esp,persona,numero,polaridad,tiempo)+' '+'"'+traduccion+'"'+' '+dcc.Markdown(**expansion**)
 
 
 if __name__ == '__main__':
